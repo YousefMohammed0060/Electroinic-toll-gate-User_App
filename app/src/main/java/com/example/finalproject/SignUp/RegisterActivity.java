74022e5086d9c4d5b2e005d9f03c3a8f0df7c3d9
YousefMohammed0060 <myousef049@gmail.com>
2022-03-30 22:07:36 +0200

    Firebase Updates

--- app/src/main/java/com/example/finalproject/SignUp/RegisterActivity.java
+++ app/src/main/java/com/example/finalproject/SignUp/RegisterActivity.java
@@ -20,7 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText NewEmail,NewPassword,ConfirmNewPassword;
+    EditText NewEmail, NewPassword, ConfirmNewPassword;
     TextView alreadyHaveAccount;
 
     FirebaseAuth mAuth;
@@ -34,51 +34,51 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void inti() {
-        NewEmail=findViewById(R.id.LoginEmail);
-        NewPassword=findViewById(R.id.LoginPassword);
-        ConfirmNewPassword=findViewById(R.id.ConfirmNewPassword);
-        alreadyHaveAccount=findViewById(R.id.alreadyHaveAccount);
+        NewEmail = findViewById(R.id.NewEmail);
+        NewPassword = findViewById(R.id.NewPassword);
+        ConfirmNewPassword = findViewById(R.id.ConfirmNewPassword);
+        alreadyHaveAccount = findViewById(R.id.alreadyHaveAccount);
         alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(RegisterActivity.this, LoginActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
 
 
-        mAuth=FirebaseAuth.getInstance();
-        mLoadingBar=new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mLoadingBar = new ProgressDialog(this);
     }
 
     public void Register(View view) {
-        String Email=NewEmail.getText().toString();
-        String Password=NewPassword.getText().toString();
-        String confirmPassword=ConfirmNewPassword.getText().toString();
+        String Email = NewEmail.getText().toString();
+        String Password = NewPassword.getText().toString();
+        String confirmPassword = ConfirmNewPassword.getText().toString();
 
-        if(Email.isEmpty() || !Email.contains("@") ||!Email.contains(".")){
-            showError(NewEmail,"Email is not valid");
-        }else if (Password.isEmpty() || Password.length()<6){
-            showError(NewPassword,"Password is too short");
-        }else if (!confirmPassword.equals(Password)){
-            showError(ConfirmNewPassword,"Password did not match!");
-        }else {
+        if (Email.isEmpty() || !Email.contains("@") || !Email.contains(".")) {
+            showError(NewEmail, "Email is not valid");
+        } else if (Password.isEmpty() || Password.length() < 6) {
+            showError(NewPassword, "Password is too short");
+        } else if (!confirmPassword.equals(Password)) {
+            showError(ConfirmNewPassword, "Password did not match!");
+        } else {
             mLoadingBar.setTitle("Registration");
             mLoadingBar.setMessage("Please wait,While check your data");
             mLoadingBar.setCanceledOnTouchOutside(false);
             mLoadingBar.show();
 
-            mAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         mLoadingBar.dismiss();
                         Toast.makeText(RegisterActivity.this, "Registration is successful", Toast.LENGTH_SHORT).show();
-                        Intent intent=new Intent(RegisterActivity.this, SetupProfileActivity.class);
+                        Intent intent = new Intent(RegisterActivity.this, SetupProfileActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
                         finish();
-                    }else {
+                    } else {
                         mLoadingBar.dismiss();
                         Toast.makeText(RegisterActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
                     }
